aaa4968f05cdd5b629cebaa46af823a0219b4585
~lojamodav <lojamodav@gmail.com>
2022-03-25 22:00:26 +0100

    Models

--- script/Models.R
+++ script/Models.R
@@ -7,36 +7,36 @@
 ##################################################################################
 
 
-
-
-
+##GAM MODELS FOR MALE HAKES
 
 ################################
-#modelo machos con ssb_age
+#GAM models for  male hakes with  SSB_age
 #################################
 
-L50ma<-as.data.frame(cbind(year,AMO,NAO,SST,BIO,ssb_age_t,Knm,L50macho))
-colnames(L50ma)<-c("year","AMO","NAO","SST","BIO","ssb_age_t","Knm","L50m")
-#backward
+L50male<-as.data.frame(cbind(Year,AMO,NAO,SST,BIO,ssb_age_t,Knm,L50_Male))
+colnames(L50male)<-c("Year","AMO","NAO","SST","BIO","ssb_age_t","Knm","L50m")
 
-m<-gam(L50m~s(year,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(SST,bs="cr",k=6)+s(ssb_age_t,bs="cr",k=6)+s(BIO,bs="cr",k=6)+s(Knm,bs="cr",k=6),method="REML",data=L50ma) #eliminmos bio
+#Procedure step backward 
 
-m<-gam(L50m~s(year,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(SST,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(Knm,bs="cr",k=7),method="REML",data=L50ma) #eliminmos sst
 
-m<-gam(L50m~s(year,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7),method="REML",data=L50ma) #eliminmos knm
+m<-gam(L50m~s(year,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(SST,bs="cr",k=6)+s(ssb_age_t,bs="cr",k=6)+s(BIO,bs="cr",k=6)+s(Knm,bs="cr",k=6),method="REML",data=L50male) #Remove BIO
 
-m<-gam(L50m~s(year,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(SST,bs="cr",k=7),method="REML",data=L50ma) #eliminmos ssb
+m<-gam(L50m~s(year,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(SST,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(Knm,bs="cr",k=7),method="REML",data=L50male) #Remove sst
 
-m<-gam(L50m~s(year,bs="cr",k=4)+s(NAO,bs="cr",k=4),method="REML",data=L50ma) #eliminmos nao
+m<-gam(L50m~s(year,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7),method="REML",data=L50male) #Remove Knm
 
-m<-gam(L50m~s(year,bs="cr",k=4),method="REML",data=L50ma) 
+m<-gam(L50m~s(year,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(SST,bs="cr",k=7),method="REML",data=L50male) #Remove SSB
+
+m<-gam(L50m~s(year,bs="cr",k=4)+s(NAO,bs="cr",k=4),method="REML",data=L50ma) #Remove NAO
+
+m<-gam(L50m~s(year,bs="cr",k=4),method="REML",data=L50male) 
 
 
 
 summary(m)
 gam.check(m)
 
-#test residual 
+#Residual testing
 
 jarque.bera.test(resid(m))
 shapiro.test(resid(m))
@@ -44,17 +44,17 @@ adfTest(resid(m))
 kpss.test(resid(m))
 Box.test(resid(m),lag=10)
 t.test(resid(m),mu=0)
-bptest(m,varformula = ~year, data=L50ma)
+bptest(m,varformula = ~Year, data=L50male)
 
 #Graphic test residuals.
 x<-c(1,3,2,4)
 n<-matrix(x,ncol=2)
 layout(n)
-plot(LjungBoxTest(resid(m))[,3],ylim=c(0,1),col="black",ylab="P-valor",xlab="Retardo")
+plot(LjungBoxTest(resid(m))[,3],ylim=c(0,1),col="black",ylab="P-valor",xlab="Lag")
 abline(h=0.05,lty=2,col="blue")
 res<-(resid(m))
 resp<-predict(m)
-plot(res~resp,type="p",xlab="Predichos",ylab="Residuals")
+plot(res~resp,type="p",xlab="Predicted",ylab="Residuals")
 abline(h=0,lty=2,col="blue")
 acf(resid(m),lag.max=38, main="ACF")
 pacf(resid(m),lag.max =38, main="PACF")
@@ -63,29 +63,29 @@ pacf(resid(m),lag.max =38, main="PACF")
 #Graphics
 
 
-plot(L50m ~ year,data=L50ma,lwd=1,col="blue",xlab="Year",ylab="Size at first maturity")
-itsadug::plot_smooth(m ,view=("year"), add=TRUE, col="lightblue", rug=FALSE, print.summary = FALSE)
+plot(L50m ~ year,data=L50male,lwd=1,col="blue",xlab="Year",ylab="Size at first maturity")
+itsadug::plot_smooth(m ,view=("Year"), add=TRUE, col="lightblue", rug=FALSE, print.summary = FALSE)
 
 
 
 ################################
-#modelo machos con ssb_ln
+#GAM models for  male hakes with SSB_Length
 #################################
 
-L50mb<-as.data.frame(cbind(year,AMO,NAO,SST,BIO,ssb_ln_t,Knm,L50macho))
-colnames(L50mb)<-c("year","AMO","NAO","SST","BIO","ssb_ln_t","Knm","L50m")
+L50male<-as.data.frame(cbind(Year,AMO,NAO,SST,BIO,ssb_ln_t,Knm,L50m))
+colnames(L50male)<-c("Year","AMO","NAO","SST","BIO","ssb_ln_t","Knm","L50m")
 
-#Step backward 
+#Procedure step backward 
 
-m<-gam(L50m ~s(year,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(SST,bs="cr",k=6)+s(ssb_ln_t,bs="cr",k=6)+s(BIO,bs="cr",k=6)+s(Knm,bs="cr",k=6),method="REML",data=L50mb) #eliminmos bio
+m<-gam(L50m ~s(Year,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(SST,bs="cr",k=6)+s(ssb_ln_t,bs="cr",k=6)+s(BIO,bs="cr",k=6)+s(Knm,bs="cr",k=6),method="REML",data=L50male) #eliminmos bio
 
-m<-gam(L50m ~s(year,bs="cr",k=4)+s(NAO,bs="cr",k=4)+s(ssb_ln_t,bs="cr",k=4)+s(Knm,bs="cr",k=4),method="REML",data=L50mb) #eliminmos sst
+m<-gam(L50m ~s(Year,bs="cr",k=4)+s(NAO,bs="cr",k=4)+s(ssb_ln_t,bs="cr",k=4)+s(Knm,bs="cr",k=4),method="REML",data=L50male) #eliminmos sst
 
-m<-gam(L50m~s(year,bs="cr",k=4)+s(NAO,bs="cr",k=4)+s(ssb_ln_t,bs="cr",k=4),method="REML",data=L50mb) #eliminmos knm
+m<-gam(L50m~s(Year,bs="cr",k=4)+s(NAO,bs="cr",k=4)+s(ssb_ln_t,bs="cr",k=4),method="REML",data=L50male) #eliminmos knm
 
-m<-gam(L50m~s(year,bs="cr",k=4)+s(NAO,bs="cr",k=4),method="REML",data=L50mb) #eliminmos ssb_ln
+m<-gam(L50m~s(Year,bs="cr",k=4)+s(NAO,bs="cr",k=4),method="REML",data=L50male) #eliminmos ssb_ln
 
-m<-gam(L50m~s(year,bs="cr",k=4),method="REML",data=L50mb) #eliminamos sst
+m<-gam(L50m~s(Year,bs="cr",k=4),method="REML",data=L50male) #eliminamos sst
 
 
 #llegamos al mismo modelo que con ssb_age, como se ha visto anteriormente,
@@ -96,30 +96,30 @@ summary(m)
 AIC(m)
 
 #######################################################
-### Talla de primera madurez para las merluzas hembra
+### ##GAM MODELS FOR FEMALE HAKES
 #######################################################
 
 
-L50ha<-as.data.frame(cbind(year,AMO,NAO,SST,BIO,ssb_age_t,Knh,L50ht)); L50ha
-colnames(L50ha)<-c("year","AMO","NAO","SST","BIO","ssb_age_t","Kn","L50hembra")
+L50female<-as.data.frame(cbind(Year,AMO,NAO,SST,BIO,ssb_age_t,Knh,L50f)); L50female
+colnames(L50female)<-c("Year","AMO","NAO","SST","BIO","ssb_age_t","Kn","L50f")
 
 ################################
-#modelo hembra con ssb_age
+#GAM models for  female hakes with  SSB_age
 #################################
 
-#procedimiento backward
+#Procedure step backward 
 
-m<-gam(L50hembra~s(year,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(SST,bs="cr",k=7)+s(BIO,k=7,bs="cr")+s(Kn,k=7,bs="cr"),method="REML",data=L50ha)#eliminamos sst
+m<-gam(L50f~s(Year,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(SST,bs="cr",k=7)+s(BIO,k=7,bs="cr")+s(Kn,k=7,bs="cr"),method="REML",data=L50female)#Remove SST
 
-m<-gam(L50hembra~s(year,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(BIO,k=7,bs="cr")+s(Kn,k=7,bs="cr"),method="REML",data=L50ha)#eliminamos kn
+m<-gam(L50f~s(Year,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(BIO,k=7,bs="cr")+s(Kn,k=7,bs="cr"),method="REML",data=L50female)#Remove Kn
 
-m1<-gam(L50hembra~s(year,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(BIO,k=7,bs="cr"),method="REML",data=L50ha)#eliminamos kn
+m<-gam(L50f~s(Year,bs="cr",k=7)+s(ssb_age_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(BIO,k=7,bs="cr"),method="REML",data=L50female)
 
 
 summary(m1)
 gam.check(m)
 
-#an?lisis residuos
+#Residual testing
 
 jarque.bera.test(resid(m))
 shapiro.test(resid(m))
@@ -127,18 +127,18 @@ Box.test(resid(m),lag=10)
 adfTest(resid(m))
 kpss.test(resid(m))
 t.test(resid(m),mu=0)
-bptest(m,varformula = ~year+ssb_age_t+NAO+BIO,data=L50ha) #corregir
+bptest(m,varformula = ~Year+ssb_age_t+NAO+BIO,data=L50female) #corregir
 
-#an?lisis gr?fico
+#Graphic residual testing
 
 x<-c(1,3,2,4)
 n<-matrix(x,ncol=2)
 layout(n)
-plot(LjungBoxTest(resid(m))[,3],ylim=c(0,1),col="black",ylab="P-valor",xlab="Retardo")
+plot(LjungBoxTest(resid(m))[,3],ylim=c(0,1),col="black",ylab="P-value",xlab="Lag")
 abline(h=0.05,lty=2,col="blue")
 res<-(resid(m))
 resp<-predict(m)
-plot(res~resp,type="p",xlab="Predichos",ylab="Residuos")
+plot(res~resp,type="p",xlab="Predicted",ylab="Residuals")
 abline(h=0,col="blue",lty=2)
 acf(resid(m),lag.max=38, main="ACF")
 pacf(resid(m),lag.max =38, main="PACF")
@@ -148,46 +148,47 @@ par(mfrow=c(2,2))
 
 par(mfrow=c(2,2))
 
-plot(L50hembra~year,data=L50ha,lwd=1,type="p",col="darkgreen",xlab="A?o", ylab="Talla de primera madurez")
-itsadug::plot_smooth(m,view = c("year"),add=TRUE,col="lightgreen",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
+plot(L50female~Year,data=L50female,lwd=1,type="p",col="darkgreen",xlab="Year", ylab="Size at first maturity")
+itsadug::plot_smooth(m,view = c("Year"),add=TRUE,col="lightgreen",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
-plot(L50hembra~ssb_age_t,data=L50ha,lwd=1,type="p",col="darkgreen",xlab="ssb_age_t", ylab="Talla de primera madurez")
+plot(L50female~ssb_age_t,data=L50female,lwd=1,type="p",col="darkgreen",xlab="ssb_age_t", ylab="Size at first maturity")
 itsadug::plot_smooth(m,view = c("ssb_age_t"),add=TRUE,col="lightgreen",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
 
-plot(L50hembra~BIO,data=L50ha,lwd=1,type="p",col="darkgreen",xlab="BIO", ylab="Talla de primera madurez")
+plot(L50female~BIO,data=L50female,lwd=1,type="p",col="darkgreen",xlab="BIO", ylab="Size at first maturity")
 itsadug::plot_smooth(m,view = c("BIO"),add=TRUE,col="lightgreen",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
-plot(L50hembra~NAO,data=L50ha,lwd=1,type="p",col="darkgreen",xlab="NAO", ylab="Talla de primera madurez")
+plot(L50female~NAO,data=L50female,lwd=1,type="p",col="darkgreen",xlab="NAO", ylab="Size at first maturity")
 itsadug::plot_smooth(m,view = c("NAO"),add=TRUE,col="lightgreen",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
 
-plot(L50ha$L50hembra,  type = "b", ylab = "Talla de primera madurez",col="darkgreen")
+plot(L50female$L50f,  type = "b", ylab = "Size at first maturity",col="darkgreen")
 lines(predict(m),col="lightgreen",lwd=2)
 
 
 ################################
-#modelo hembra con ssb_ln
+#Female model with  SSB_length
 #################################
 
 
-L50ha<-as.data.frame(cbind(year,AMO,NAO,SST,BIO,ssb_ln_t,Knh,L50ht)); L50ha
-colnames(L50ha)<-c("year","AMO","NAO","SST","BIO","ssb_ln_t","Kn","L50hembra")
+L50female<-as.data.frame(cbind(Year,AMO,NAO,SST,BIO,ssb_ln_t,Knh,L50f)); L50female
+colnames(L50female)<-c("Year","AMO","NAO","SST","BIO","ssb_ln_t","Kn","L50f")
 
-# backward step
+#Procedure step backward 
 
 
-m<-gam(L50_Female~s(Year,bs="cr",k=6)+s(ssb_ln_t,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(SST,bs="cr",k=6)+s(BIO,k=6,bs="cr")+s(Kn,k=6,bs="cr"),method="REML",data=L50ha)#eliminamos sst
+m<-gam(L50f~s(Year,bs="cr",k=6)+s(ssb_ln_t,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(SST,bs="cr",k=6)+s(BIO,k=6,bs="cr")+s(Kn,k=6,bs="cr"),method="REML",data=L50female)#Remove SST
 
-m<-gam(L50hembra~s(Year,bs="cr",k=6)+s(ssb_ln_t,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(BIO,k=6,bs="cr")+s(Kn,k=6,bs="cr"),method="REML",data=L50ha)#eliminamos kn
+m<-gam(L50f~s(Year,bs="cr",k=6)+s(ssb_ln_t,bs="cr",k=6)+s(NAO,bs="cr",k=6)+s(BIO,k=6,bs="cr")+s(Kn,k=6,bs="cr"),method="REML",data=L50female)#Remove Kn
 
-m<-gam(L50hembra~s(Year,bs="cr",k=7)+s(ssb_ln_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(BIO,k=7,bs="cr"),method="REML",data=L50ha)
+m<-gam(L50f~s(Year,bs="cr",k=7)+s(ssb_ln_t,bs="cr",k=7)+s(NAO,bs="cr",k=7)+s(BIO,k=7,bs="cr"),method="REML",data=L50female)
 
 
 
 summary(m)
-AIC(m)
 gam.check(m)
+
+#Residual testing
 
 jarque.bera.test(resid(m))
 shapiro.test(resid(m))
@@ -195,16 +196,19 @@ Box.test(resid(m),lag=10)
 adfTest(resid(m))
 kpss.test(resid(m))
 t.test(resid(m),mu=0)
-bptest(m,varformula = ~year+ssb_ln_t+NAO+BIO,data=L50ha)
+bptest(m,varformula = ~Year+ssb_ln_t+NAO+BIO,data=L50female)
+
+
+#Graphic residual testing
 
 x<-c(1,3,2,4)
 n<-matrix(x,ncol=2)
 layout(n)
-plot(LjungBoxTest(resid(m))[,3],ylim=c(0,1),col="black",ylab="P-valor",xlab="Retardo")
+plot(LjungBoxTest(resid(m))[,3],ylim=c(0,1),col="black",ylab="P-value",xlab="Lag")
 abline(h=0.05,lty=2,col="blue")
 res<-(resid(m))
 resp<-predict(m)
-plot(res~resp,type="p",xlab="Predichos",ylab="Residuos")
+plot(res~resp,type="p",xlab="Predicted",ylab="Residuals")
 abline(h=0,col="blue",lty=2)
 acf(resid(m),lag.max=38, main="ACF")
 pacf(resid(m),lag.max =38, main="PACF")
@@ -215,20 +219,23 @@ getwd()
 setwd("C:/Users/Usuario/Desktop/paper/PAPER/gráficos")
 ggsave("Knh.jpeg",scale = 1,dpi=300)
 
+
+#Plor models
+
 par(mfrow=c(2,2))
-plot(L50h~year,data=L50ha,lwd=1,type="p",col=6,xlab="Year", ylab="Size at first maturity")
-itsadug::plot_smooth(m,view = c("year"),add=TRUE,col="lightpink",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
+plot(L50f~Year,data=L50female,lwd=1,type="p",col=6,xlab="Year", ylab="Size at first maturity")
+itsadug::plot_smooth(m,view = c("Year"),add=TRUE,col="lightpink",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
 
-plot(L50h~ssb_ln_t,data=L50ha,lwd=1,type="p",col= 6,xlab="SSB at length", ylab="Size at first maturity")
+plot(L50f~ssb_ln_t,data=L50female,lwd=1,type="p",col= 6,xlab="SSB at length", ylab="Size at first maturity")
 itsadug::plot_smooth(m,view = c("ssb_ln_t"),add=TRUE,col="lightpink",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
 
-plot(L50h~NAO,data=L50ha,lwd=1,type="p",col=6,xlab="NAO", ylab="Size at first maturity")
+plot(L50f~NAO,data=L50female,lwd=1,type="p",col=6,xlab="NAO", ylab="Size at first maturity")
 itsadug::plot_smooth(m,view = c("NAO"),add=TRUE,col="lightpink",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
 
-plot(L50h~BIO,data=L50ha,lwd=1,type="p",col=6,xlab="Biomass", ylab="Size at first maturity")
+plot(L50f~BIO,data=L50female,lwd=1,type="p",col=6,xlab="Biomass", ylab="Size at first maturity")
 itsadug::plot_smooth(m,view = c("BIO"),add=TRUE,col="lightpink",rug=FALSE,rm.ranef = TRUE,print.summary = FALSE)
 
 dev.off()
